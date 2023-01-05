01b61a26372d6c70a5ce6d59cd7fb06367037d2b
thomasam000 <thomasam000@gmail.com>
2023-01-04 22:12:39 -0500

    added map and one sidewalk segment to the map

---
+++ sidewalks/src/assets/Sidewalk(2).js
@@ -0,0 +1,3 @@
+export default {"type":"FeatureCollection", "features": [
+{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[40.693347637980075, -73.98509701855721],[40.693349914782075, -73.98516909242731],[40.69308927116378, -73.98518251463591],[40.69308650736223, -73.98511175202414],[40.693347637980075, -73.98509701855721]]]},"properties":{"shape_area":"1886.24685238","shape_leng":"229.91218499","feat_code":"3800","status":"Unchanged","sub_code":"380000","source_id":"21380000165"}},
+]}
--- sidewalks/src/components/HelloWorld.vue
+++ sidewalks/src/components/HelloWorld.vue
@@ -1,42 +1,68 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="container">
+      <div id="mapContainer" style="position:absolute; top:100px; bottom:0; left:0; right:0;">
+      </div>
+    </div>
+   </div>
 </template>
 
 <script>
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import jsonData from '../assets/Sidewalk(2).js'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    // LMap, LTileLayer
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 13,
+      center: [40.701452, -73.967923],
+      mapDiv: null,
+      polygons: []
+    }
+  },
+  methods: {
+    setupLeafletMap () {
+      // const mapDiv = L.map('mapContainer').setView(this.center, 13)
+      this.mapDiv = L.map('mapContainer', {
+        center: this.center,
+        zoom: this.zoom
+      })
+      L.tileLayer(this.url, {
+        attribution: this.attribution
+        // subdomains: ['a', 'b', 'c']
+      }).addTo(this.mapDiv)
+      // L.tileLayer(
+      //   'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      //   {
+      //     attribution:
+      //       'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      //     maxZoom: 18,
+      //     id: 'mapbox/streets-v11',
+      //     accessToken: 'XXX'
+      //   }
+      // ).addTo(mapDiv)
+    }
+  },
+  mounted () {
+    this.setupLeafletMap()
+    console.log(jsonData.features[0])
+    jsonData.features.forEach(shape => {
+      var polygon = L.polygon(shape.geometry.coordinates, {color: 'blue'}).addTo(this.mapDiv);
+      this.polygons.push(polygon)
+    })
+    this.mapDiv.fitBounds(this.polygons[0].getBounds());
+
   }
 }
 </script>
